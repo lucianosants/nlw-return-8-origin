c072a48160de4d5a28f4595e67767a61ba43c42a
lucianosants <lucianola18@gmail.com>
2022-05-06 20:28:32 -0300

    stage 5 final

--- main.js
+++ main.js
@@ -4,6 +4,35 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+  activeMenuAtCurrentSection(home)
+  activeMenuAtCurrentSection(services)
+  activeMenuAtCurrentSection(about)
+  activeMenuAtCurrentSection(contact)
+}
+
+function activeMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2 
+
+  const sectionTop = section.offsetTop
+  
+  const sectionHeight = section.offsetHeight
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if(sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
